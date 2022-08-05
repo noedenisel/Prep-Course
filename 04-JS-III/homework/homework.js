e3e323fe77242aa4408c201a59a8f78863c36423
noedenisel <noelialombardo@hotmail.com>
2022-08-05 13:57:04 -0300

    did more exercices

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -117,22 +117,38 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
-}
+
+
+  var masGrande = Math.max.apply(null, numeros);
+  return masGrande
+
+  }
+  
+
 
 
 function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
-  // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
+  // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplementæe devuélvelo
   // Escribe tu código aquí:
+
+  var productos = 1;
+  if (arguments.length === 0) {
+    return 0;
+  } else if (arguments.length === 1) {
+    return arguments[0];
+  } else {
+    for (i= 0; i < arguments.length; i++){
+      productos = productos * arguments[i];
+    }
+  } return productos;
 }
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-for (let i = 0 ; i < arreglo.length; i++){
- 
-} return i > 18;
+
 }
 
 
